819362b138e351908ea88b3cb382bd926cc40d3d
UWONG990 <renoutomo990@gmail.com>
2025-04-22 20:07:07 +0700

    Token fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -27,7 +27,7 @@ exports.login = async (req, res) => {
     if (!user) return res.status(400).json({ msg: "Invalid credentials" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+    if (!isMatch) return res.status(400).json({ msg: "Wrong password" });
 
     const token = jwt.sign({ user: { id: user.id } }, process.env.JWT_SECRET, { expiresIn: "1d" });
     res.json({ token });
